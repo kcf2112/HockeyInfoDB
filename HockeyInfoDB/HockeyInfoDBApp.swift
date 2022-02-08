e04dc33931b28d0cb36dfa1d55aafa81fa2f2bb3
Kevin Filer <kcf2112@gmail.com>
2022-02-08 18:21:48 -0500

    Initial Commit

---
+++ HockeyInfoDB/HockeyInfoDBApp.swift
@@ -0,0 +1,17 @@
+//
+//  HockeyInfoDBApp.swift
+//  HockeyInfoDB
+//
+//  Created by Kevin Filer on 2/8/22.
+//
+
+import SwiftUI
+
+@main
+struct HockeyInfoDBApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
